cbd712db7deee68c90202f54399a9e59659ef3b6
novakvova <novakvova@gmail.com>
2017-11-29 20:28:41 +0200

    Begin work create class Photo

---
+++ SimpleGalleryWpfApp/Photo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace SimpleGalleryWpfApp
+{
+    public class Photo
+    {
+        private string _path;
+        private Uri _source;
+        private BitmapFrame _image;
+        public Photo(string path)
+        {
+            _path = path;
+            _source = new Uri(path);
+            _image = BitmapFrame.Create(_source);
+        }
+
+        public override string ToString()
+        {
+            return _source.ToString();
+        }
+
+        public string Source { get { return _path; } }
+        public BitmapFrame Image { get { return _image; } set { _image = value; } }
+
+
+        
+    }
+}
