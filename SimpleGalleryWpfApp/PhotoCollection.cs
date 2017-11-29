3d5b5d3151a4b2d662a7c675db6ef8a69c020097
novakvova <novakvova@gmail.com>
2017-11-29 20:53:23 +0200

    Add Class PhotoCollection

---
+++ SimpleGalleryWpfApp/PhotoCollection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGalleryWpfApp
+{
+    public class PhotoCollection : ObservableCollection<Photo>
+    {
+        private DirectoryInfo _directory;
+        public PhotoCollection()    {    }
+
+        public PhotoCollection(string path) : this(new DirectoryInfo(path))    {  }
+        public PhotoCollection(DirectoryInfo directory)
+        {
+            _directory = directory;
+            Update();
+        }
+        public string Path
+        {
+            set
+            {
+                _directory = new DirectoryInfo(value);
+                Update();
+            }
+            get { return _directory.FullName; }
+
+        }
+        public DirectoryInfo Directory
+        {
+            set
+            {
+                _directory = value;
+                Update();
+            }
+            get { return _directory; }
+        }
+
+        private void Update()
+        {
+            this.Clear();
+            try
+            {
+                foreach (FileInfo f in _directory.GetFiles("*.jpg"))
+                {
+                    Add(new Photo(f.FullName));
+                }
+            }
+            catch (DirectoryNotFoundException)
+            {
+                System.Windows.MessageBox.Show("No Such Directory");
+            }
+        }
+
+    }
+}
